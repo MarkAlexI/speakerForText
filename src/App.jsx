4742145e0091969bb838f68f1b6a20660eae8208
markovaleksandr108@gmail.com <markovaleksandr108@gmail.com>
2023-04-18 11:38:48 +0300

    Added controller for speed of speech.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Container from 'reactstrap/lib/Container';
 import { Voices } from '@/Voices';
+import { SpeechSpeed } from '@/SpeechSpeed';
 import '/src/assets/styles/style.css';
 
 export const App = () => {
@@ -8,6 +9,7 @@ export const App = () => {
   return (
     <Container>
       <Voices />
+      <SpeechSpeed />
     </Container>
   )
 };
---
+++ src/components/SpeechSpeed.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+import Row from 'reactstrap/lib/Row';
+import Col from 'reactstrap/lib/Col';
+import FormGroup from 'reactstrap/lib/FormGroup';
+import Label from 'reactstrap/lib/Label';
+import Input from 'reactstrap/lib/Input';
+
+export const SpeechSpeed = () => {
+  const [speed, setSpeed] = useState(1);
+  const handleChange = (event) => {
+    setSpeed(event.target.value);
+  };
+  
+  return (
+    <Row>
+      <Col className="col-6">
+        <FormGroup>
+          <Label for="rate">
+            Speed (<span className="rate-value">{speed}</span>)
+          </Label>
+          <Input
+            type="range"
+            className="form-control-range"
+            id="rate"
+            min="0.5"
+            max="3"
+            value={speed}
+            step="0.1"
+            onChange={handleChange}
+          ></Input>
+        </FormGroup>
+      </Col>
+    </Row>
+  )
+};
