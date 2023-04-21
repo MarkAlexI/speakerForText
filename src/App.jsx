6d87a048b0fc772294a256885f0419699f425f2d
markovaleksandr108@gmail.com <markovaleksandr108@gmail.com>
2023-04-21 08:54:42 +0300

    Connected to speech synthesis.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+
 import Container from 'reactstrap/lib/Container';
+
 import { Voices } from '@/Voices';
 import { SpeechSpeed } from '@/SpeechSpeed';
 import { PitchRate } from '@/PitchRate';
 import { TextArea } from '@/TextArea';
+
+import { speechSynthesis } from './utils/speechSynthesis';
+
 import '/src/assets/styles/style.css';
 
 export const App = () => {
-  
+  console.log(speechSynthesis);
   return (
     <Container>
       <Voices />
